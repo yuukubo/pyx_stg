a988948178ef17e33f5c7ec3c1f94ef8ceb11794
yuukubo <i.yusuke.shk@gmail.com>
2020-01-17 01:19:21 +0900

    add game board. test download link.

--- pyx_stg.py
+++ pyx_stg.py
@@ -4,12 +4,19 @@ class App:
     def __init__(self):
         pyxel.init(160, 120, caption="pyx_stg")
 
-        self.x = 50
-        self.y = 100
+        self.text_area_w = 50
+
+        self.game_board_x = 10
+        self.game_board_y = 10
+        self.game_board_w = pyxel.width - (self.text_area_w + self.game_board_x * 2)
+        self.game_board_h = pyxel.height - (self.game_board_y * 2)
+
+        self.x = (self.game_board_w + self.game_board_x) / 2
+        self.y = self.game_board_h * 9 / 10
         self.w = 4
         self.h = 4
-        self.spd = 2
-        self.title = "pyx_stg c1"
+        self.spd = 1
+        self.title = "pyx_stg c2"
 
         pyxel.run(self.update, self.draw)
 
@@ -24,12 +31,23 @@ class App:
             self.y = self.y + 1 * self.spd
         elif pyxel.btnp(pyxel.KEY_Q):
             pyxel.quit()
+        
+        if self.x < self.game_board_x:
+            self.x = self.game_board_x
+        if (self.game_board_w + self.game_board_x) < (self.x + self.w):
+            self.x = self.game_board_w + self.game_board_x - self.w
+        if self.y < self.game_board_y:
+            self.y = self.game_board_y
+        if (self.game_board_h + self.game_board_y) < (self.y + self.h):
+            self.y = self.game_board_h + self.game_board_y - self.h
 
     def draw(self):
         pyxel.cls(0)
         pyxel.rect(0, 0, pyxel.width, pyxel.height, 2)
-        pyxel.rect(10, 10, 90, 100, 0)
-        pyxel.text(pyxel.width - 50, 10, self.title, 10)
+        pyxel.rect(self.game_board_x, self.game_board_y, self.game_board_w, self.game_board_h, 0)
+        pyxel.text(pyxel.width - self.text_area_w, 10, self.title, 10)
+        pyxel.text(pyxel.width - self.text_area_w, 10 * 3, "x : " + str(self.x), 10)
+        pyxel.text(pyxel.width - self.text_area_w, 10 * 4, "y : " + str(self.y), 10)
 
         pyxel.rect(self.x, self.y, self.w, self.h, 8)
 
